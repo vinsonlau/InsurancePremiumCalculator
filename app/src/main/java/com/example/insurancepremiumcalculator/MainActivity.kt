ae8c2d507ee48017ca5fe61a232485471b99e28a
vinsonlau <vinsonlwp-wm17@student.tarc.edu.my>
2020-01-14 16:07:17 +0800

    Done Insurance Premium Calculator!

--- app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
+++ app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.insurancepremiumcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,29 +16,70 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinnerAge.setOnItemClickListener(this)
+        //spinnerAge.setOnItemClickListener(this)
 
         buttonCalculate.setOnClickListener {
             calculatePremium()
         }
+
+        buttonReset.setOnClickListener {
+            spinnerAge.setSelection(0)
+            radioGroupGender.clearCheck()
+            checkBoxSmoker.setChecked(false)
+        }
     }
 
     private fun calculatePremium(){
-        val position = spinnerAge.selectedItemPosition
+        val agePosition = spinnerAge.selectedItemPosition
         val gender = radioGroupGender.checkedRadioButtonId
+        val smoker = checkBoxSmoker.isChecked
 
-        var premium: Int
+        var premium: Int = 0
 
-        if(position == 1){
-            premium = 60
+        if(agePosition == 0){
+            premium += 60
+        }else if(agePosition == 1){
+            premium += 70
+            if(gender == R.id.radioButtonMale){
+                premium += 50
+            }
+            if(smoker){
+                premium += 100
+            }
+        }else if(agePosition == 2){
+            premium += 90
+            if(gender == R.id.radioButtonMale){
+                premium += 100
+            }
+            if(smoker){
+                premium += 150
+            }
+        }else if(agePosition == 3){
+            premium += 120
+            if(gender == R.id.radioButtonMale){
+                premium += 150
+            }
+            if(smoker){
+                premium += 200
+            }
+        }else if(agePosition == 4){
+            premium += 150
+            if(gender == R.id.radioButtonMale){
+                premium += 200
+            }
+            if(smoker){
+                premium += 250
+            }
+        }else if(agePosition == 5){
+            premium += 150
+            if(gender == R.id.radioButtonMale){
+                premium += 200
+            }
+            if(smoker){
+                premium += 300
+            }
         }
 
-        if(gender == R.id.radioButtonMale){
-            //TODO: calculate premium for male
-        }
-
-        if(checkBoxSmoker.isChecked){
-            //TODO: calculate premium for smoker
-        }
+        textViewPremium.setText("RM " + premium.toString())
     }
 }
